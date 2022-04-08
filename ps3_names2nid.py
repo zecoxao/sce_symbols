f7d78677185e2476448e70ef7b20ae4aa1538578
zecoxao <zecoxao@hotmail.com>
2022-04-08 18:09:44 +0100

    add ps3 nids.txt generator

---
+++ ps3_names2nid.py
@@ -0,0 +1,34 @@
+import sys, os
+import struct
+from hashlib import sha1
+import hashlib
+from base64 import b64encode as base64enc
+from binascii import unhexlify as uhx
+
+#ref https://github.com/SocraticBliss/ps4_name2nid_plugin
+
+NEW_NIDS = {}
+AEROLIB  = 'nids.txt'
+NAMES   = 'ps3_names.txt'
+
+def name2nid(name):
+    symbol = sha1(name.encode() + uhx(b'6759659904250490566427499489741A')).digest()
+    nid = struct.unpack('<I', symbol[:4])[0]
+    NEW_NIDS[nid]=name
+
+def save_nids(NIDS):
+    csvFile=open(AEROLIB,"w")
+    for nid, name in sorted(NIDS.items(), key=lambda x: x[1]):
+        csvFile.writelines('0x%08X %s\n' % (nid, name))
+    csvFile.close()
+
+
+
+f = open(NAMES,"r")
+for line in f.readlines():
+    line = line.strip()
+    name2nid(line)
+
+f.close()
+
+save_nids(NEW_NIDS)
